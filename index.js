12ecf08db0da93fd23c2a7734c16e4585c7bded0
dnievas <nievas.david@gmail.com>
2020-04-02 10:46:27 -0300

    Limpieza de codigo.

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 const logger = require('./logger').logger;
 const db = require('./db');
 const poll = require('./poll');
-const sim = require('./sim'); // Modulo temporal para simular fichadas. Remover en prod
 
 async function main(){
     try {
         // Establecemos las conexiones a mongo y sqlserver
         const mongoDB = await db.connectMongoDB();
         const sqlPool = await db.connectSQLServerDB();
-        // Simulamos las fichadas. Solo para testing del script
-        // sim.simulateFichadas(sqlPool);
         // Sincronizamos
         poll.syncFichadas(sqlPool);
     
@@ -17,6 +14,5 @@ async function main(){
         logger.error(err);
     }
 }
-
 
 main();
